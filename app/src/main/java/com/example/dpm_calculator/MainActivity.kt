dc65797d7467f20e0644b1044f141bdf97ab4a19
EverKlett <evertonklettenberg@gmail.com>
2023-06-28 21:52:11 -0300

    Correção dos erros de build e config

--- app/src/main/java/com/example/dpm_calculator/MainActivity.kt
+++ app/src/main/java/com/example/dpm_calculator/MainActivity.kt
@@ -1,16 +1,11 @@
 package com.example.dpm_calculator
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.dpm_calculator.databinding.ActivityMainBinding
-import com.example.dpm_calculator.ui.theme.DPMCalculatorTheme
+import com.example.dpm_calculator.app.R
+import com.example.dpm_calculator.app.databinding.ActivityMainBinding
 
 @Suppress("UNUSED_PARAMETER")
 class MainActivity : ComponentActivity() {
@@ -27,17 +22,13 @@ class MainActivity : ComponentActivity() {
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
     }
 
-    fun numberAction(view: View) {
-
-        if (view is Button)
-            Log.d("numberAction", view.text.toString())
-
-        Log.d("view is Button", (view is Button).toString())
-        Log.d("mainBinding.workingsTV", activityMainBinding.workingsTV.text.toString())
-        if (view is Button) {
-
-            if (view.text == ".") {
-                if (canAddDecimal)
+    fun numberAction(view: View)
+    {
+        if(view is Button)
+        {
+            if(view.text == ".")
+            {
+                if(canAddDecimal)
                     activityMainBinding.workingsTV.append(view.text)
 
                 canAddDecimal = false
@@ -47,52 +38,136 @@ class MainActivity : ComponentActivity() {
 
             canAddOperation = true
         }
-
-        Log.d("mainBinding.workingsTV", activityMainBinding.workingsTV.text.toString())
     }
 
-    fun operationAction(view: View) {
-
-        if (view is Button)
-            Log.d("operationAction", view.text.toString())
-
-        Log.d("mainBinding.workingsTV", activityMainBinding.workingsTV.text.toString())
-
-        if (view is Button && canAddOperation) {
+    fun operationAction(view: View)
+    {
+        if(view is Button && canAddOperation)
+        {
             activityMainBinding.workingsTV.append(view.text)
             canAddOperation = false
             canAddDecimal = true
         }
     }
 
-    fun allClearAction(view: View) {
+    fun allClearAction(view: View)
+    {
         activityMainBinding.workingsTV.text = ""
         activityMainBinding.resultsTV.text = ""
     }
 
-    fun equalsAction(view: View) {}
-
-    fun backSpaceAction(view: View) {
-
+    fun backSpaceAction(view: View)
+    {
         val length = activityMainBinding.workingsTV.length()
-
-        if (length > 0)
-            activityMainBinding.workingsTV.text.subSequence(0, length - 1)
+        if(length > 0)
+            activityMainBinding.workingsTV.text = activityMainBinding.workingsTV.text.subSequence(0, length - 1)
     }
 
-    @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
+    fun equalsAction(view: View)
+    {
+        activityMainBinding.resultsTV.text = calculateResults()
     }
 
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        DPMCalculatorTheme {
-            Greeting("Android")
+    private fun calculateResults(): String
+    {
+        val digitsOperators = digitsOperators()
+        if(digitsOperators.isEmpty()) return ""
+
+        val timesDivision = timesDivisionCalculate(digitsOperators)
+        if(timesDivision.isEmpty()) return ""
+
+        val result = addSubtractCalculate(timesDivision)
+        return result.toString()
+    }
+
+    private fun addSubtractCalculate(passedList: MutableList<Any>): Float
+    {
+        var result = passedList[0] as Float
+
+        for(i in passedList.indices)
+        {
+            if(passedList[i] is Char && i != passedList.lastIndex)
+            {
+                val operator = passedList[i]
+                val nextDigit = passedList[i + 1] as Float
+                if (operator == '+')
+                    result += nextDigit
+                if (operator == '-')
+                    result -= nextDigit
+            }
         }
+
+        return result
+    }
+
+    private fun timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any>
+    {
+        var list = passedList
+        while (list.contains('x') || list.contains('/'))
+        {
+            list = calcTimesDiv(list)
+        }
+        return list
+    }
+
+    private fun calcTimesDiv(passedList: MutableList<Any>): MutableList<Any>
+    {
+        val newList = mutableListOf<Any>()
+        var restartIndex = passedList.size
+
+        for(i in passedList.indices)
+        {
+            if(passedList[i] is Char && i != passedList.lastIndex && i < restartIndex)
+            {
+                val operator = passedList[i]
+                val prevDigit = passedList[i - 1] as Float
+                val nextDigit = passedList[i + 1] as Float
+                when(operator)
+                {
+                    'x' ->
+                    {
+                        newList.add(prevDigit * nextDigit)
+                        restartIndex = i + 1
+                    }
+                    '/' ->
+                    {
+                        newList.add(prevDigit / nextDigit)
+                        restartIndex = i + 1
+                    }
+                    else ->
+                    {
+                        newList.add(prevDigit)
+                        newList.add(operator)
+                    }
+                }
+            }
+
+            if(i > restartIndex)
+                newList.add(passedList[i])
+        }
+
+        return newList
+    }
+
+    private fun digitsOperators(): MutableList<Any>
+    {
+        val list = mutableListOf<Any>()
+        var currentDigit = ""
+        for(character in activityMainBinding.workingsTV.text)
+        {
+            if(character.isDigit() || character == '.')
+                currentDigit += character
+            else
+            {
+                list.add(currentDigit.toFloat())
+                currentDigit = ""
+                list.add(character)
+            }
+        }
+
+        if(currentDigit != "")
+            list.add(currentDigit.toFloat())
+
+        return list
     }
 }
